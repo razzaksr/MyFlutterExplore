31c524dea69defe7dde9427d8cc00252d7c6b518
razzaksr <razzaksr@gmail.com>
2023-11-12 21:54:31 +0530

    Stateful widget iterating list items as column children [] in beginning

--- read_poc/lib/main.dart
+++ read_poc/lib/main.dart
@@ -5,9 +5,113 @@ void main() {
     // stateless widget
     // home: ReadCard(),
     //stateful widget
-    home: ReadCard(),
+    // home: ReadCard(),
+
+    // Listing items from List type
+    home: IllustrationList(),
   ));
 }
+
+class IllustrationList extends StatefulWidget {
+  const IllustrationList({super.key});
+
+  @override
+  State<IllustrationList> createState() => _IllustrationListState();
+}
+
+class _IllustrationListState extends State<IllustrationList> {
+
+  List<String> skillsets=[
+    'MERN Stack','Flutter Development','Java Stack','Scrum master'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Zealous Tech Corp Expert'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('images/razak.png'),
+              radius: 50.0,
+            ),
+          ),
+          Divider(
+            height: 100.0,
+            color: Colors.lightBlue,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text('Expert Name',style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.normal
+                  ),),
+                  SizedBox(height: 10.0,),
+                  Text('Razak Mohamed S',style: TextStyle(color: Colors.lightBlue,
+                      fontWeight: FontWeight.bold,fontSize: 25.0),),
+                  Text('Expert Designation',style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.normal
+                  ),),
+                  SizedBox(height: 10.0,),
+                  Text('L&D Manager',style: TextStyle(color: Colors.lightBlue,
+                      fontWeight: FontWeight.bold,fontSize: 25.0),),
+                  SizedBox(height: 10.0,),
+                  Text('Expert skills',style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.normal
+                  ),),
+                  SizedBox(height: 10.0,),
+                  // Text('$skillsets',style: TextStyle(color: Colors.lightBlue,
+                  //     fontWeight: FontWeight.bold,fontSize: 25.0),),
+                  Column(
+                    children: skillsets.map((e) => Text('$e',style:TextStyle(color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,fontSize: 25.0),)).toList(),
+                  ),
+                  Text('Expert Email',style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.normal
+                  ),),
+                  Row(
+                    children: [
+                      Icon(Icons.email_outlined),
+                      SizedBox(height:10.0),
+                      Text('razaksrmd@gmail.com',style: TextStyle(color: Colors.lightBlue,
+                          fontWeight: FontWeight.bold,fontSize: 25.0),),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            //skillsets.map((e) => Text(e)).toList();
+          });
+        },
+        child: Icon(Icons.view_agenda,color: Colors.white70,),
+        backgroundColor: Colors.lightBlue,
+      ),
+    );
+  }
+}
+
 
 class ReadCard extends StatefulWidget {
   const ReadCard({super.key});
