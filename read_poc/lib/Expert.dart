c47565693a9f50fe9feb789c111513a1fae2d2dc
razzaksr <razzaksr@gmail.com>
2023-11-12 23:09:25 +0530

    Card demo with Custom Expert object

---
+++ read_poc/lib/Expert.dart
@@ -0,0 +1,7 @@
+class Expert{
+  String expertName;
+  String expertDesignation;
+  List<dynamic> expertSkills;
+  int experience;
+  Expert(this.expertName,this.expertDesignation,this.expertSkills,this.experience);
+}
--- read_poc/lib/main.dart
+++ read_poc/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'SkillSet.dart';
+import 'Expert.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,9 +13,69 @@ void main() {
     // home: IllustrationList(),
 
     // Listing user defined class objects from List type
-    home:IllustrationListWithCustomClass()
+    // home:IllustrationListWithCustomClass()
+
+    // Listing expert objects as list
+    home: CardDemoWithListOfCustomObjects(),
   ));
 }
+
+class CardDemoWithListOfCustomObjects extends StatefulWidget {
+  const CardDemoWithListOfCustomObjects({super.key});
+
+  @override
+  State<CardDemoWithListOfCustomObjects> createState() => _CardDemoWithListOfCustomObjectsState();
+}
+
+class _CardDemoWithListOfCustomObjectsState extends State<CardDemoWithListOfCustomObjects> {
+
+  List<dynamic> myExperts=[
+    Expert("Razak Mohamed S", "L&D Manager", ['java','python','flutter'], 10),
+    Expert("Rasheedha R", "Operations Head", ['Scrum Master','digital marketing'], 10),
+    Expert("Annamalai S", "Lead Trainer", ['java','python','java script'], 5),
+  ];
+
+  Widget myExpertCard(expert){
+    return Card(
+      margin: EdgeInsets.fromLTRB(14.0, 14.0, 14.0, 0),
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(expert.expertName,style: TextStyle(fontWeight: FontWeight.bold),),
+                Text(expert.expertDesignation,style: TextStyle(fontStyle: FontStyle.italic),),
+                Text(expert.experience.toString()+'',style: TextStyle(fontStyle: FontStyle.normal),),
+              ],
+            ),
+            // Column(
+            //   children: expert.expertSkills.map((k)=>Text(k)).toList(),
+            // )
+            Text(expert.expertSkills.toString()+"")
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View Experts of Zealous'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        children: myExperts.map((e) => myExpertCard(e)).toList(),
+      ),
+    );
+  }
+}
+
 
 class IllustrationListWithCustomClass extends StatefulWidget {
   const IllustrationListWithCustomClass({super.key});
